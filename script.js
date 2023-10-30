598ac17d42a30adefe749b4ffd70653dc431e315
booshrat <booshrat@gmail.com>
2023-10-30 12:32:22 +0000

    Nearly finished

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 const fruitForm = document.querySelector("#inputSection form")
-console.log(fruitForm);
+fruitForm.addEventListener("submit", extractFruit)
 
+function extractFruit(e) {
+    e.preventDefault()
+    // console.log(e)
+    // console.log(e.target[0].value)  // best use
+    // console.log(e.target.fruitInput.value)
+
+    addFruit(e.target[0].value)
+
+    e.target[0].value = "" // clears the form after submitting
+
+
+
+}
+
+const fruitList = document.querySelector("#fruitSection ul")
+function addFruit(fruit) {
+    if (!fruit) {
+        console.log("Invalid fruit");
+    } else {
+        const li = document.createElement("li")
+        li.textContent = fruit
+        fruitList.appendChild(li)
+    }
+}
